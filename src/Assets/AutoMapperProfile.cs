e43987b11d1c669b86410335d657f835f15d42e0
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2020-03-16 14:00:24 +0300

    Automapper profile fixed.

--- src/Assets/AutoMapperProfile.cs
+++ src/Assets/AutoMapperProfile.cs
@@ -1,7 +1,9 @@
+using System.Globalization;
 using Assets.Client.Models.AssetPairs;
 using Assets.Client.Models.Assets;
 using Assets.Domain.Entities;
 using AutoMapper;
+using Google.Protobuf.WellKnownTypes;
 
 namespace Assets
 {
@@ -13,9 +15,21 @@ namespace Assets
 
             CreateMap<AssetPair, AssetPairModel>(MemberList.Source);
 
-            CreateMap<Asset, Service.Assets.Contracts.Asset>(MemberList.Destination);
+            CreateMap<Asset, Service.Assets.Contracts.Asset>(MemberList.Destination)
+                .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created.ToTimestamp()))
+                .ForMember(dest => dest.Modified, opt => opt.MapFrom(src => src.Modified.ToTimestamp()));
 
-            CreateMap<AssetPair, Service.Assets.Contracts.AssetPair>(MemberList.Destination);
+            CreateMap<AssetPair, Service.Assets.Contracts.AssetPair>(MemberList.Destination)
+                .ForMember(dest => dest.MinVolume,
+                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.MaxVolume,
+                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.MaxOppositeVolume,
+                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.MarketOrderPriceThreshold,
+                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                .ForMember(dest => dest.Created, opt => opt.MapFrom(src => src.Created.ToTimestamp()))
+                .ForMember(dest => dest.Modified, opt => opt.MapFrom(src => src.Modified.ToTimestamp()));
         }
     }
 }
