0407a6ec57085b46388079424cf93b0f083ae472
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2020-03-17 11:24:39 +0300

    Asset pair contract mapping fixed.

--- src/Assets/AutoMapperProfile.cs
+++ src/Assets/AutoMapperProfile.cs
@@ -24,11 +24,11 @@ namespace Assets
                 .ForMember(dest => dest.MinVolume,
                     opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.MaxVolume,
-                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                    opt => opt.MapFrom(src => src.MaxVolume.ToString(CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.MaxOppositeVolume,
-                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                    opt => opt.MapFrom(src => src.MaxOppositeVolume.ToString(CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.MarketOrderPriceThreshold,
-                    opt => opt.MapFrom(src => src.MinVolume.ToString(CultureInfo.InvariantCulture)))
+                    opt => opt.MapFrom(src => src.MarketOrderPriceThreshold.ToString(CultureInfo.InvariantCulture)))
                 .ForMember(dest => dest.Created, opt => opt.MapFrom(src => Convert(src.Created)))
                 .ForMember(dest => dest.Modified, opt => opt.MapFrom(src => Convert(src.Modified)));
         }
